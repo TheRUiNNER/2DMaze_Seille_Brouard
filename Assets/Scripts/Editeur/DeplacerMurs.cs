10afa98e4338039baaaed81d126bfb1cbbaf9dc2
AlexisBrouardYnov <67193389+AlexisBrouardYnov@users.noreply.github.com>
2021-04-06 13:47:48 +0200

    06/04

---
+++ Assets/Scripts/Editeur/DeplacerMurs.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeplacerMurs : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            Debug.Log("Pressed primary button.");
+
+        if (Input.GetMouseButtonDown(1))
+            Debug.Log("Pressed secondary button.");
+
+        if (Input.GetMouseButtonDown(2))
+            Debug.Log("Pressed middle click.");
+    }
+}
