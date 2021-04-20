f3cea4cd09daff6c3b2b3c73794d5b5945a3e5f6
AlexisBrouardYnov <67193389+AlexisBrouardYnov@users.noreply.github.com>
2021-04-20 13:41:56 +0200

    Correction bugs IA

--- Assets/Scripts/IHM/IA.cs
+++ Assets/Scripts/IHM/IA.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework.Constraints;
@@ -5,68 +6,73 @@ using UnityEngine;
 
 public class IA : MonoBehaviour
 {
-    public float speed;
+    public Rigidbody2D rb;
+    private Vector2 dir;
+    
+    public float speed = 5f;
     private float horizontalMovement;
     private float verticalMovement;
 
     private bool verifMove;
+    private bool isMoving;
     
     private Transform target;
     public SpriteRenderer graphics;
     public Transform waypoint;
     
-    public Rigidbody2D rb;
-    private Vector3 velocity = Vector3.zero;
-
-
     void Start()
     {
         target = waypoint;
         graphics = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
-        if (target.position.x > transform.position.x)
-        {
-            horizontalMovement = 0.5f;
-        }
-        if (target.position.x < transform.position.x)
-        {
-            horizontalMovement = -0.5f;
-        }
-
-        if (target.position.x == transform.position.x)
-        {
-            horizontalMovement = 0f;
-        }
-
-        if (target.position.y > transform.position.y)
-        {
-            verticalMovement = 0.5f;
-        }
-        if (target.position.y < transform.position.y)
-        {
-            verticalMovement = -0.5f;
-        }
-
-        if (target.position.y == transform.position.y)
-        {
-            verticalMovement = 0;
-        }
-
-        MoveIA(horizontalMovement,verticalMovement);
-    }
-
-    void MoveIA(float _horizontalMovement, float _verticalMovement)
-    {
-        if(verifMove)
-        {
-            transform.position = new Vector2(transform.position.x + _horizontalMovement,transform.position.y + _verticalMovement);
-        }
+        moveIA();
     }
 
     public void getMove()
     {
         verifMove = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Test");
+    }
+
+    void moveIA()
+    {
+        if (target.position.x > transform.position.x)
+        {
+            dir.x = 1;
+        }
+        if (target.position.x <= transform.position.x)
+        {
+            dir.x = -1;
+        }
+
+        if (Mathf.Abs(target.position.x - transform.position.x) < 0.05f)
+        {
+            dir.x = 0;
+        }
+        
+        if (target.position.y > transform.position.y)
+        {
+            dir.y = 1;
+        }
+        if (target.position.y <= transform.position.y)
+        {
+            dir.y = -1;
+        }
+        if (Mathf.Abs(target.position.y - transform.position.y) < 0.05f)
+        {
+            dir.y = 0;
+        }
+
+        if (verifMove)
+        {
+            rb.MovePosition(rb.position + dir * speed * Time.fixedDeltaTime);
+        }
     }
 }
